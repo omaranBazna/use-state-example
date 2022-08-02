ddf2b9afac9a04473067617011232fe01d8661c7
omaran bazna <omran4d@gmail.com>
2022-08-01 22:30:11 -0400

    use sate with out array deconstraction

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [value, setValue] = useState("hello");
+  const value = useState("hello")[0];
+  const setValue = useState("hello")[1];
   return (
     <div
       onClick={() => {
